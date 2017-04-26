adbf0e7f3210151c2a5d11218be9f3f32b85b6a0
mff <happymff@126.com>
2017-04-26 17:53:25 +0800

    add RainTest

---
+++ src/main/java/Rainfall.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mff on 2017/4/26.
+ */
+public class Rainfall {
+    public static double mean(String town, String strng) {
+        // your code
+        double mean=0;
+
+        return  mean;
+    }
+    public static double variance(String town, String strng) {
+        // your code
+        double variance=0;
+
+        return  variance;
+    }
+}
