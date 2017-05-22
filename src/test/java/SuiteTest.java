091fd34d0cc2c88b6919c5a37aaea8247e776649
mengfeifei <happymff@126.com>
2017-05-22 23:17:32 +0800

    add BinaryTest3

--- src/test/java/SuiteTest.java
+++ src/test/java/SuiteTest.java
@@ -12,15 +12,15 @@ public class SuiteTest {
 
     @Test
     public void test1() {
-        assertEquals(1.275, Suite.going(62), DELTA);
+        assertEquals(1.275, Suite.going(5), DELTA);
     }
-//    @Test
-//    public void test2() {
-//        assertEquals(1.2125, Suite.going(6), DELTA);
-//    }
-//    @Test
-//    public void test3() {
-//        assertEquals(1.173214, Suite.going(7), DELTA);
-//    }
+    @Test
+    public void test2() {
+        assertEquals(1.2125, Suite.going(6), DELTA);
+    }
+    @Test
+    public void test3() {
+        assertEquals(1.173214, Suite.going(7), DELTA);
+    }
 
 }
